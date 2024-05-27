6fbe2aaf8c802b1a2e90424bf3cff0eeae03895d
Calvin <cl.kleijn@student.avans.nl>
2024-05-27 23:06:53 +0200

    + Dependency injection container

---
+++ GameplanAPI/DependencyInjection.cs
@@ -0,0 +1,52 @@
+﻿using FluentValidation;
+using GameplanAPI.Features.Competition._Helpers;
+using GameplanAPI.Features.Competition._Interfaces;
+using GameplanAPI.Features.Competition;
+using GameplanAPI.Features.Season;
+using GameplanAPI.Features.Season._Helpers;
+using GameplanAPI.Features.Season._Interfaces;
+using GameplanAPI.Shared.Abstractions;
+using GameplanAPI.Shared.Abstractions.Handling;
+using GameplanAPI.Shared.Abstractions.Interfaces;
+using GameplanAPI.Shared.Database.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace GameplanAPI
+{
+    public static class DependencyInjection
+    {
+        public static void Configure(WebApplicationBuilder builder)
+        {
+            var configuration = builder.Configuration;
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("ApplicationConnectionString")));
+
+            builder.Services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
+            {
+                builder
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                    .WithOrigins("http://localhost:4200");
+            }));
+
+            builder.Services.AddScoped<ICompetitionRepository, CompetitionRepository>();
+            builder.Services.AddScoped<ICompetitionMapper, CompetitionMapper>();
+            builder.Services.AddScoped<ISeasonRepository, SeasonRepository>();
+            builder.Services.AddScoped<ISeasonMapper, SeasonMapper>();
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+            builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            builder.Services.AddLogging(cfg => cfg.AddConsole());
+
+            builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+            builder.Services.AddProblemDetails();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddControllers();
+        }
+    }
+}
